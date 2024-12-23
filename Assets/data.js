fcf5a2c2aefb7746e0c6efc99be0932b0464df9b
choijake0207 <jake.ch0712@gmail.com>
2024-12-23 14:55:43 -0800

    add data source js file

---
+++ Assets/data.js
@@ -0,0 +1,55 @@
+
+
+
+export const projects = [
+    {
+        id: 0,
+        projectName: "Echo-v1",
+        stack: ["React.js", "Express.js", "Node.js", "MySQL", "CSS"],
+        extStack: [
+            "Rest API", "JWT"
+        ],
+        summary: "Full-stack social media application where users are able to post, like, comment on, and save media",
+        extSummary: "Echo",
+        liveStatus: false,
+        imageURL: "./Assets/images/Echo-demo.png",
+        liveLink: null,
+        gitLink: "https://github.com/choijake0207/EchoV1",
+    },
+    
+    {
+        id: 1,
+        projectName: "Tasker",
+        stack: ["HTML", "CSS", "JavaScript"],
+        extStack: ["@dnd-kit"],
+        summary: "A client-side personal KanBan board allowing users to create boards, customize columns, and drag-drop items.",
+        liveStatus: true,
+        imageURL: "./Assets/images/TaskerDemo.png",
+        liveLink: "https://choijake0207.github.io/Tasker/",
+        gitLink: "https://github.com/choijake0207/Tasker",
+
+    },
+    {
+        id: 2,
+        projectName: "Habit.io",
+        stack: ["React.js", "Express.js", "Node.js", "MySQL", "CSS"],
+        extStack: ["Rest API"],
+        summary: "Full-stack habit management platform allowing users to create, pause, and reset habits and duration-based goals. Users are able to view all habit data including number of resets, number of completed goals, and more.",
+        liveStatus: false,
+        imageURL: "./Assets/images/HabitDemo.png",
+        liveLink: null,
+        gitLink: "https://github.com/choijake0207/Habit.io"
+    },
+    {
+        id: 3,
+        projectName: "Lift-4-Dummies",
+        stack: ["HTML", "CSS", "JavaScript"],
+        extStack: null,
+        summary: "A client-side project designed to allow users to learn about fitness techniques in an interactive method through the use of clickable SVGs.",
+        liveStatus: true,
+        imageURL: "./Assets/images/LiftDemo.png",
+        liveLink: null,
+        gitLink: "https://github.com/choijake0207/Fit4Dummies"
+    }
+   
+]
