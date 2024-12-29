80db901b4fa4ff31a2f49d27a340456bce140e5f
choijake0207 <jake.ch0712@gmail.com>
2024-12-28 18:18:34 -0800

    add skills data

--- Assets/data.js
+++ Assets/data.js
@@ -4,15 +4,16 @@
 export const projects = [
     {
         id: 0,
-        projectName: "Echo-v1",
+        projectName: "Echo",
         stack: ["React.js", "Express.js", "Node.js", "MySQL", "CSS"],
         extStack: [
             "Rest API", "JWT"
         ],
+        cardDesc: "Social media app",
         summary: "Full-stack social media application where users are able to post, like, comment on, and save media",
         extSummary: "Echo",
         liveStatus: false,
-        imageURL: "./Assets/Echo-demo.png",
+        imageURL: "./Assets/EchoDemo.png",
         liveLink: null,
         gitLink: "https://github.com/choijake0207/EchoV1",
     },
@@ -22,6 +23,7 @@ export const projects = [
         projectName: "Tasker",
         stack: ["HTML", "CSS", "JavaScript"],
         extStack: ["@dnd-kit"],
+        cardDesc: "Personal kanban board",
         summary: "A client-side personal KanBan board allowing users to create boards, customize columns, and drag-drop items.",
         liveStatus: true,
         imageURL: "./Assets/TaskerDemo.png",
@@ -34,6 +36,7 @@ export const projects = [
         projectName: "Habit.io",
         stack: ["React.js", "Express.js", "Node.js", "MySQL", "CSS"],
         extStack: ["Rest API"],
+        cardDesc: "Habit management app",
         summary: "Full-stack habit management platform allowing users to create, pause, and reset habits and duration-based goals. Users are able to view all habit data including number of resets, number of completed goals, and more.",
         liveStatus: false,
         imageURL: "./Assets/HabitDemo.png",
@@ -45,6 +48,7 @@ export const projects = [
         projectName: "Lift-4-Dummies",
         stack: ["HTML", "CSS", "JavaScript"],
         extStack: null,
+        cardDesc: "Guide for weight lifting",
         summary: "A client-side project designed to allow users to learn about fitness techniques in an interactive method through the use of clickable SVGs.",
         liveStatus: true,
         imageURL: "./Assets/LiftDemo.png",
@@ -52,4 +56,70 @@ export const projects = [
         gitLink: "https://github.com/choijake0207/Fit4Dummies"
     }
    
+]
+
+export const skills = [
+
+    {
+        id: 0,
+        name: "React.js",
+        logo: "",
+        type: "fwlib"
+    },
+    {
+        id: 1,
+        name: "HTML",
+        logo: "",
+        type: "lang"
+    }, 
+    {
+        id: 2,
+        name: "CSS",
+        logo: "",
+        type: "lang"
+    }, 
+    {
+        id: 3,
+        name: "JavaScript",
+        logo: "",
+        type: "lang"
+    },
+    {
+        id: 4,
+        name: "Express.js",
+        logo: "",
+        type: "fwlib"
+    },
+    {
+        id: 5,
+        name: "Node.js",
+        logo: "",
+        type: "fwlib"
+    },
+    {
+        id: 6,
+        name: "Sequelize",
+        logo: "",
+        type: "fwlib"
+    },
+    {
+        id: 7,
+        name: "Jest",
+        logo: "",
+        type: "fwlib"
+    },
+    {
+        id: 8,
+        name: "Git",
+        logo: "",
+        type: "tool"
+    },
+    {
+        id: 9,
+        name: "MySQL",
+        logo: "",
+        type: "tool"
+    }
+
+
 ]
