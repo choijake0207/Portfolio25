bf1b5d32bafeffd12ecdd52da2965239b442f2b0
choijake0207 <jake.ch0712@gmail.com>
2025-02-27 12:06:33 -0800

    comment out dark mode toggling , remove modal toggling and card hover effects

--- main.js
+++ main.js
@@ -1,52 +1,32 @@
 import { projects, skills } from "./Assets/data.js"
-// Light Dark Mode
 const body = document.querySelector(".body")
-let darkMode = true       
-body.classList.add(darkMode ? "dark" : "light")
-const toggleBTN = document.querySelector(".light-dark-toggle")
-const toggleIcon = document.querySelector(".light-dark-icon")
-
-function toggleDarkMode () {
-    if (!darkMode) {
-        toggleIcon.src = "./Assets/icons/moon-stars-bold.svg"
-        body.classList.remove("dark")
-        body.classList.add("light")
-    } else {
-        toggleIcon.src = "./Assets/icons/sun-bold.svg"
-        body.classList.remove("light")
-        body.classList.add("dark")
-    }
-}
-
-toggleBTN.addEventListener("click", () => {
-    darkMode = !darkMode
-    toggleDarkMode()
-})
 
 
+// let darkMode = true       
+// body.classList.add(darkMode ? "dark" : "light")
+// const toggleBTN = document.querySelector(".light-dark-toggle")
+// const toggleIcon = document.querySelector(".light-dark-icon")
 
-// // Tab Selection //
-// const tabs = document.querySelectorAll(".tab")
+// function toggleDarkMode () {
+//     if (!darkMode) {
+//         toggleIcon.src = "./Assets/icons/moon-stars-bold.svg"
+//         body.classList.remove("dark")
+//         body.classList.add("light")
+//     } else {
+//         toggleIcon.src = "./Assets/icons/sun-bold.svg"
+//         body.classList.remove("light")
+//         body.classList.add("dark")
+//     }
+// }
 
-// const pages = document.querySelectorAll(".tab-content")
-// tabs.forEach(tab => {
-//     tab.addEventListener("click", () => {
-
-//         const tabType = tab.dataset.tab
-
-//         pages.forEach(page => {
-//             page.classList.remove("active-page")
-//         })
-
-//         tabs.forEach(tab => {
-//             tab.classList.remove("active-tab")
-//         })
-
-//         tab.classList.add("active-tab")
-
-//         document.getElementById(tabType).classList.add("active-page")
-//     })
+// toggleBTN.addEventListener("click", () => {
+//     darkMode = !darkMode
+//     toggleDarkMode()
 // })
+
+
+
+
 
 // Projects Page
 const projectPage = document.querySelector(".project-list")
@@ -60,45 +40,29 @@ function createProjectCard(project) {
     card.classList.add("card", `${project.projectName}`)
     card.innerHTML = ` 
         <div class="ss-wrap">
-            <img class="screenshot" src=${project.imageURL}>
+            ${project.id === 0 ? `<p>On Its Way</p>` : ""}
+            <img class="screenshot" src=${project.imageURL}></img>
         </div>
-        <h2 class="card-title">${project.projectName}</h2>
-        <p class="card-desc">${project.cardDesc}</p>
-
+        <div class="card-body">
+            <div class="card-header">
+                <h2 class="card-title">${project.projectName}</h2>
+                <div class="card-links-container">
+                    <a target="_blank" href=${project.liveLink} class=${project.liveLink ? "demo-link" : "disabled-link"}>
+                        ${project.liveLink ? "Demo" : "Coming Soon"}
+                    </a>
+                    <a target="_blank" href=${project.gitLink} class="code-link">Code</a>
+                </div>
+            </div>
+            <p class="card-desc">${project.cardDesc}</p>
+            <ul class="tech-list">
+                ${project.stack.map(tech => `<li class="tech-item">${tech}</li>`).join("")}
+            </ul>
+        </div>
     `
-
-    const hoverWrap = document.createElement("div")
-    hoverWrap.classList.add("hover-wrap")
-    const hoverBTN = document.createElement("button")
-    hoverBTN.classList.add("modal-button")
-    hoverBTN.textContent = "View More"
-    hoverBTN.addEventListener("click", () => toggleModal(project.id))
-    card.addEventListener("mouseover", (event) => handleCardEvent(event, hoverWrap))
-    card.addEventListener("mouseout", (event) => handleCardEvent(event, hoverWrap))
-
-    hoverWrap.appendChild(hoverBTN)
-    card.appendChild(hoverWrap)
     projectPage.appendChild(card)
 }
 
-function handleCardEvent(event, hoverWrap) {
-    switch (event.type) {
-        case "mouseover":
-            hoverWrap.classList.add("hover-visible")
-            break;
-        case "mouseout":
-            if (!event.relatedTarget || !event.currentTarget.contains(event.relatedTarget)) {    
-                hoverWrap.classList.remove("hover-visible")
-                hoverWrap.classList.add("hover-invisible")
-                setTimeout(() => {
-                    hoverWrap.classList.remove("hover-invisible")
-                }, 500)
-            }
-            break;
-    }
-}
 
-// Project Modal
 
 const modal = document.createElement("div")
 modal.classList.add("modal")
